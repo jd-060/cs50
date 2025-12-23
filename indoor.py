3e4bf51c55903f3a437d298423273aeda73306c2
jd-060 <jmdahm@yahoo.com>
2025-12-22 18:46:42 -0800

    initial commit

--- indoor.py
+++ indoor.py
@@ -1,6 +1,5 @@
 # Ask user for a simple clause (a string)
-#UserStatement = input ("Tell me a quick detail about yourself: ")
-UserStatement = 'hello world'
+UserStatement = input ("Tell me a quick detail about yourself: ")
 
 # Remove leading/trailing spaces, and convert all letters to lower case
 UserStatement = UserStatement.strip().lower()
